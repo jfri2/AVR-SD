59f85bd2d0c508672847a92d418c536c3924713d
jfritz022 <jfritz022@gmail.com>
2015-06-24 17:33:16 -0400

    Things

---
+++ FATFS-OLD/ASF_Mod/ASF_Mod.c
@@ -0,0 +1,17 @@
+/*
+ * ASF_Mod.c
+ *
+ * Created: 6/20/2015 7:21:13 PM
+ *  Author: John
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ FatFs_ATmega328p/FatFs_ATmega328p.c
@@ -0,0 +1,20 @@
+/*
+ * FatFs_ATmega328p.c
+ *
+ * Created: 6/21/2015 4:18:59 PM
+ *  Author: John
+ */ 
+
+#define F_CPU 16000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "serial.c"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
